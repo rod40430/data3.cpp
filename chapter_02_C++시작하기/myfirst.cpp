f35186b08cd6daf356e4b6035c64e91ac29cd878
xogns <xogns40430@naver.com>
2023-09-09 22:37:20 +0900

    메세지를 출력한다

---
+++ chapter_02_C++시작하기/myfirst.cpp
@@ -0,0 +1,13 @@
+// myfirst.cpp -- 메세지를 출력한다
+
+#include <iostream>
+
+int main(void)
+{
+  using namespace std;
+  cout<<"C++의 세계로 오십시오.";
+  cout<<endl;
+  cout<<"후회하지 않으실 겁니다!"<<endl;
+
+  return 0;
+}
