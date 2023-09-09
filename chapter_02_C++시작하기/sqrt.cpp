0280df71ac74cae64265f089a6c171317a0c57c0
xogns <xogns40430@naver.com>
2023-09-09 23:38:52 +0900

    sqrt() 함수를 사용한다

---
+++ chapter_02_C++시작하기/sqrt.cpp
@@ -0,0 +1,20 @@
+// sqrt.cpp -- sqrt() 함수를 사용한다
+#include <iostream>
+#include <cmath>  // 또는 math.h
+
+int main(void)
+{
+  using namespace std;
+
+  double area;
+  cout<<"마루 면적을 평방피트 단위로 입력하시오 : ";
+  cin>>area;
+
+  double side;
+  side = sqrt(area);
+  cout<<"사각형 마루라면 한 변이 "<< side
+      <<"피트에 상당합니다."<<endl;
+  cout<<"멋지네요!"<<endl;
+
+  return 0;
+}
