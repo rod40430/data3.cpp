c3445ce147102660e7b80f163309af5af01ceb06
xogns <xogns40430@naver.com>
2023-09-22 09:30:40 +0900

    수치 입력 뒤에 오는 문자열 입력

---
+++ chapter_04_복합_데이터형/numstr.cpp
@@ -0,0 +1,19 @@
+// numstr.cpp -- 수치 입력 뒤에 오는 문자열 입력
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  cout<<"지금 사시는 아파트에 언제 입주하셨습니까?\n";
+  int year;
+  (cin>>year).get();
+  cout<<"사시는 도시를 말씀해 주시겠습니까?\n";
+  char address[80];
+  cin.getline(address, 80);
+  cout<<"아파트 입주 연도 : "<<year<<endl;
+  cout<<"도시 : "<<address<<endl;
+  cout<<"등록이 완료되었습니다!\n";
+  
+  return 0;
+}
