d35cf4ec998f28c62621af5cff04d148bd4ebf21
xogns <xogns40430@naver.com>
2023-09-28 22:51:15 +0900

    for 루프

---
+++ chapter_05_루프와 관계 표현식/forloop.cpp
@@ -0,0 +1,15 @@
+// forloop.cpp -- for 루프
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  int i;
+  // 초기화; 조건 검사; 갱신
+  for (i = 0; i < 5; i++)
+    cout<<"C++가 루프를 사용합니다.\n";
+  cout<<"루프를 언제 끝내야 하는지 C++는 알고 있습니다.\n";
+  
+  return 0;
+}
