365583b2ae66cfb5579bec5b53bf9553d963dc46
xogns <xogns40430@naver.com>
2023-09-29 23:53:30 +0900

    do while문을 for문으로 표현
    본인의 프로그래밍 능력을 자랑하기보단 알기쉬운 코드를 작성하는게 일반적으론 더 좋다. (하지만 나도 가끔 이런 식으로 아는 만큼 보이는 코드를 작성하고 싶다)

---
+++ chapter_05_루프와 관계 표현식/DifferentFor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void)
+{
+  int i = 0;
+
+  for(;;i++)  // do while문을 for문으로 표현
+    {
+      // do something...
+
+      if (30 >= i) break;
+    }
+
+  return 0;
+}
+// 본인의 프로그래밍 능력을 자랑하기보단 알기쉬운 코드를 작성하는게 일반적으론 더 좋다.
