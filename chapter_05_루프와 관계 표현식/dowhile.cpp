7c2cbe0a68995b68a47594152640471edb6bd89d
xogns <xogns40430@naver.com>
2023-09-29 23:47:21 +0900

    탈출 조건 루프

---
+++ chapter_05_루프와 관계 표현식/dowhile.cpp
@@ -0,0 +1,20 @@
+// dowhile.cpp --  탈출 조건 루프
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  int n;
+
+  cout<<"1부터 10까지의 수 중에서 ";
+  cout<<"내가 좋아하는 수를 한번 맞추어 보십시오.\n";
+
+  do
+    {
+      cin>>n;  // 먼저 루프 몸체를 실행한다
+    } while (n != 7);  // 나중에 조건을 검사한다
+  cout<<"맞았습니다. 내가 좋아하는 수는 럭키 세븐 7입니다.\n";9
+  
+  return 0;
+}
