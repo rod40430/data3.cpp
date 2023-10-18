c8f1bfc7b1aaddbd6dbe66d309b393bc1cc0b4f3
xogns <xogns40430@naver.com>
2023-10-18 20:49:12 +0900

    외부 링크와 내부 링크를 가지는 변수

---
+++ chapter_09_메모리 모델과 이름 공간/외부 링크 내부 링크/twofile2.cpp
@@ -0,0 +1,17 @@
+// twofile2.cpp -- 외부 링크와 내부 링크를 가지는 변수
+#include <iostream>
+
+using namespace std;
+
+extern int tom;  // tom은 다른 파일에 정의되어 있다
+static int dick = 10;  // 외부 dick의 앞을 가린다
+int harry = 200;  // 외부 변수를 정의한다
+                  // twofile1.cpp의 harry와 충돌하지 않는다
+
+void remote_access(void)
+{
+  cout<<"remote_access()가 보고하는 세 변수의 주소 : \n";
+  cout<<&tom<<" = &tom\n";
+  cout<<&dick<<" = &dick\n";
+  cout<<&harry<<" = &harry\n";
+}
