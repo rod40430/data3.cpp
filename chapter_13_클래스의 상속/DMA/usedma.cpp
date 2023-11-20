bfe28f102094d4469aa13de6b3d4eecc399013c6
xogns <xogns40430@naver.com>
2023-11-20 23:26:52 +0900

    상속, 프렌드, 동적 메모리 대입

---
+++ chapter_13_클래스의 상속/DMA/usedma.cpp
@@ -0,0 +1,31 @@
+// usedma.cpp -- 상속, 프렌드, 동적 메모리 대입
+// dma.cpp와 함께 컴파일한다
+#include "dma.hpp"
+
+int main(void)
+{
+  using std::cout;
+  using std::endl;
+
+  baseDMA shirt("Portabelly", 8);
+  lacksDMA balloon("red", "Blimpo", 4);
+  hasDMA map("Mercator", "Buffalo Keys", 5);
+
+  cout<<"baseDMA 객체를 출력한다 : \n";
+  cout<<shirt<<endl;
+  cout<<"lacksDMA 객체를 출력한다 : \n";
+  cout<<balloon<<endl;
+  cout<<"hasDMA 객체를 출력한다 : \n";
+  cout<<map<<endl;
+
+  lacksDMA balloon2(balloon);
+  cout<<"lacksDMA 복사 결과 : \n";
+  cout<<balloon2<<endl;
+
+  hasDMA map2;
+  map2 = map;
+  cout<<"hasDMA 대입 결과 : \n";
+  cout<<map2<<endl;
+
+  return 0;
+}
