5778a43a6410b186b465b7316e0b72da07076d9f
xogns <xogns40430@naver.com>
2023-12-28 20:55:35 +0900

    STL을 string에 적용한다

---
+++ chapter_16_string 클래스와_표준 템플릿 라이브러리/strgst1.cpp
@@ -0,0 +1,23 @@
+// strgst1.cpp -- STL을 string에 적용한다
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main(void)
+{
+  using namespace std;
+  string letters;
+  cout<<"글자 그룹을 입력하십시오 (끝내려면 quit) : ";
+  while (cin>>letters && letters != "quit")
+    {
+      cout<<letters<<"의 모든 치환들 : "<<endl;
+      sort(letters.begin(), letters.end());
+      cout<<letters<<endl<<endl;
+      while (next_permutation(letters.begin(), letters.end()))
+        cout<<letters<<endl;
+      cout<<"다음 시퀀스를 입력하십시오 (끝내려면 quit) : ";
+    }
+  cout<<"프로그램을 종료합니다.\n";
+
+  return 0;
+}
