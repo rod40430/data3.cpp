7aaefe656c5e521261f5ace11aab688dfa6954af
xogns <xogns40430@naver.com>
2024-01-02 20:35:39 +0900

    정밀도를 설정한다

---
+++ chapter_17_입력, 출력, 파일/cout/precise.cpp
@@ -0,0 +1,19 @@
+// precise.cpp -- 정밀도를 설정한다
+#include <iostream>
+
+int main(void)
+{
+  using std::cout;
+
+  float price1 = 20.40;
+  float price2 = 1.9 + 8.0 / 9.0;
+
+  cout<<"\"손오공 인형\""<<price1<<"!\n";
+  cout<<"\"사오정 인형\""<<price2<<"!\n";
+
+  cout.precision(2);
+  cout<<"\"손오공 인형\""<<price1<<"!\n";
+  cout<<"\"사오정 인형\""<<price2<<"!\n";
+  
+  return 0;
+}
