ebc8ea9f8090f19fd8c67d0753cc766dec90fb33
xogns <xogns40430@naver.com>
2024-01-06 23:58:54 +0900

    입력 행이 갈 때 get()을 사용하여 자른다

---
+++ chapter_17_입력, 출력, 파일/cin/truncate.cpp
@@ -0,0 +1,32 @@
+// truncate.cpp -- 입력 행이 갈 때 get()을 사용하여 자른다
+#include <iostream>
+
+namespace 
+{
+  const int SLEN = 10;
+}
+inline void eatline() {while (std::cin.get() != '\n') continue;}
+
+int main(void)
+{
+  using std::cout;
+  using std::endl;
+  using std::cin;
+
+  char name[SLEN];
+  char title[SLEN];
+  cout<<"이름을 입력하십시오 : ";
+  cin.get(name, SLEN);
+  if (cin.peek() != '\n')
+    cout<<"죄송합니다. 이름란이 좁아서"<<name<<"만 적어 넣었습니다."<<endl;
+  eatline();
+  cout<<name<<"씨! 직위를 입력하십시오 : \n";
+  cin.get(title, SLEN);
+  if (cin.peek() != '\n')
+    cout<<"직위도 뒷부분을 잘랐습니다.\n";
+  eatline();
+  cout<<"이름 : "<<name
+    <<"\n직위 : "<<title<<endl;
+
+  return 0;
+}
