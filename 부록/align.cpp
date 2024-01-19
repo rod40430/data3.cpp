8346ebdc3d2b040edebc16af9761dfa81dad697d
xogns <xogns40430@naver.com>
2024-01-19 23:24:48 +0900

    checking alignment
    선언 순서에 따른 구조 크기

---
+++ 부록/align.cpp
@@ -0,0 +1,34 @@
+// align.cpp -- checking alignment
+#include <iostream>
+
+using namespace std;
+
+struct things1
+{
+  char ch;
+  int a;
+  double x;
+};
+
+struct things2
+{
+  int a;
+  double x;
+  char ch;
+};
+
+int main(void)
+{
+  things1 th1;
+  things2 th2;
+  
+  cout<<"char aligment : "<<alignof(char)<<endl;
+  cout<<"int aligment : "<<alignof(int)<<endl;
+  cout<<"double aligment : "<<alignof(double)<<endl;
+  cout<<"things1 aligment : "<<alignof(things1)<<endl;
+  cout<<"things2 aligment : "<<alignof(things2)<<endl;
+  cout<<"things1 size : "<<sizeof(things1)<<endl;
+  cout<<"things2 size : "<<sizeof(things2)<<endl;
+
+  return 0;
+}
